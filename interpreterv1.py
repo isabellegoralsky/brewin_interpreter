5a28694b4ad92a1b0a360438d2d12c864d7c0a78
Isabelle Goralsky <isabelle.goralsky@gmail.com>
2023-10-15 20:56:25 -0700

    impl but not fcalls

--- interpreterv1.py
+++ interpreterv1.py
@@ -12,11 +12,11 @@ class Interpreter(InterpreterBase):
         # each item in list is a different line
 
         # use parser to parse the source code into AST
-            # will always start with main function
-            # main fucntion will have 1+ statements inside
-                # assignment and printing are statements that are OK
+        # will always start with main function
+        # main fucntion will have 1+ statements inside
+        # assignment and printing are statements that are OK
         ast = parse_program(program)
-        self.var_to_val = {} # need self???
+        self.variable_name_to_value = {} # need self???
        
         # main func node = get main func node (ast)
         # is program node guaranteed to be first???
@@ -29,41 +29,69 @@ class Interpreter(InterpreterBase):
         # ??? need this
 
         #run_func(main_func_node)
-        run_func(main_func_node)
+        self.run_func(main_func_node)
 
         # process nodes of the AST to run the program
     
-    def run_func(func_node):
+    def run_func(self, func_node):
         # run statements in order that they appear in the program
         # aka order of execution
         for statement_node in func_node.get('statements'):
-			run_statement(statement_node)
+            self.run_statement(statement_node)
 
-    def run_statement(statement_node):
+    def run_statement(self, statement_node):
         # look inside the statement nodes and figure out how to tell what they are
         if statement_node.elem_type == "=": # assignment
-            do_assignment(statement_node)
-        elif statement_node.elem_type == InterpreterBase.FCALL_DEF: # function call
-            do_func_call(statement_node)
-# 76485
-    def do_assignment(statement_node):
-        pass
-        # target_var_name = get_target_variable_name(statement_node)
-		# source_node = get_expression_node(statement_node)
-		# resulting_value = evaluate_expression(source_node)
-		# this.variable_name_to_value[target_var_name] = resulting_value
+            self.do_assignment(statement_node)
+        #elif statement_node.elem_type == InterpreterBase.FCALL_DEF: # function call
+        #do_func_call(statement_node)
 
-    def do_func_call(statement_node):
-         pass
+    def do_assignment(self, statement_node):
+        target_var_name = statement_node.get('name')
+        # in x = 2 + 10 this is x
+        source_node = statement_node.get('expression')
+        # either expression like 2+10, value like 2, or var like x=y
+        resulting_value = self.evaluate_expression(source_node)
+        self.variable_name_to_value[target_var_name] = resulting_value
+
+    #def do_func_call(self, statement_node):
+    #pass
     
-    def evaluate_expression(expression_node):
-        pass
-        #if is_value_node(expression_node):
-            # return get_value(expression_node)
-        # else if is_variable_node(expression_node):
-            # return get_value_of_variable(expression_node)
-        # else if is_binary_operator(expression_node):
-            # return evaluate_binary_operator(expression_node)
-        # ...
+    def evaluate_expression(self, node):
+        if node.elem_type == InterpreterBase.STRING_DEF or node.elem_type == InterpreterBase.INT_DEF:
+            # value node
+            return self.get_value(node)
+        elif node.elem_type == InterpreterBase.VAR_DEF:
+            # variable node
+            return self.get_value_of_variable(node)
+        elif node.elem_type == '+' or node.elem_type == '-':
+            # expression node representing binary operation
+            return self.evaluate_binary_operator(node)
+            # elif node.elem_type == "fcall":
+            # eval it
     
-    # more funcs...
+    # value of value node
+    def get_value(self, val_node):
+        return val_node.get('val')
+    
+    # value of var node
+    def get_value_of_variable(self, var_node):
+        return self.variable_name_to_value[var_node.get('name')]
+    
+    # binary op + or - eval
+    def evaluate_binary_operator(self, expression_node):
+        # dict holds op1 and op2
+        op1 = expression_node.get('op1')
+        op2 = expression_node.get('op2')
+
+        op1 = self.evaluate_expression(op1)
+        op1 = self.evaluate_expression(op2)
+
+        if expression_node.elem_type == '+':
+            return op1 + op2
+        else: # subtraction
+            return op1 - op2
+
+          
+
+    # STARTING THESE BY THROWING NO ERRS
